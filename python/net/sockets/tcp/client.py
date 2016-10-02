844512347e95e86285faa206e81f62b1f9090f0a
gilson27 <gilsonvarghese7@gmail.com>
2016-10-02 19:41:26 +0530

    Todays revision start update

---
+++ python/net/sockets/tcp/client.py
@@ -0,0 +1,21 @@
+"""
+    @author Gilson Varghese
+    @date October 2 2016
+    @description tcp socket client example
+"""
+
+"""
+    Class to create connection to server 
+    @class
+"""
+class SocketClient(object):
+    def __init__(self):
+        print("--in Socket Client Constructor")
+
+
+"""
+    Create object of class and connect to server 
+    if the python program is executed 
+"""
+if(__name__ == "__main__"):
+    objSocketClient = SocketClient()
---
+++ python/net/sockets/tcp/server.py
@@ -0,0 +1,19 @@
+"""
+    @author Gilson Varghese
+    @date October 2 2016
+    @description tcp server socket example
+"""
+
+class SocketServer(object):
+    def __init__(self):
+        print("--in Socket Server Constructor")
+
+
+"""
+    Create object of class and start server 
+    if the python program is executed 
+"""
+if(__name__ == "__main__"):
+    objSocketServer = SocketServer()
+
+
