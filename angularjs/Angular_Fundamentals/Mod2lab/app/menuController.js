2d60719a55efe424d2d3edc65740fa54515f769a
Gilson Varghese <gilsonvarghese7@gmail.com>
2016-12-18 18:16:06 +0530

    Added Angular js module2 tasks

--- angularjs/Angular_Fundamentals/Mod2lab/app/menuController.js
+++ angularjs/Angular_Fundamentals/Mod2lab/app/menuController.js
@@ -1,5 +1,16 @@
 app.controller('menuController', ["$scope", 
     function($scope) {
         $scope.menu = "home";
+        $scope.model = {"title": "Our Menu"};
+        $scope.model.mainDish = 'BBQ Chicken Pizza'
+		$scope.$watch('model.mainDish', function (newValue, oldValue) {
+			if (newValue === 'BBQ Chicken Pizza') {
+				alert('You have selected the BBQ Chicken Pizza!');
+			}
+		});
+        
+        $scope.changeMainDish = function(item) {
+			$scope.model.mainDish = item;
+		};
     }
 ]);
