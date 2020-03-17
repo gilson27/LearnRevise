3576991ad038283a37ea968adbdb849a953639e1
gilsonvarghese12 <gilson.varghese@ignitarium.com>
2020-03-17 15:40:56 +0530

    Added matlab scripts from coursera

---
+++ matlab/octave_biginner_edx/scripts/sphere_volume.m
@@ -0,0 +1,5 @@
+%The script computes volume of sphere for a given radius
+Radius = input("Enter radius of the sphere");
+Volume = 4/3 * pi * Radius^3;
+disp("Volume of the sphere of radius");
+disp([num2str(Radius) ' is ' num2str(Volume)]);
